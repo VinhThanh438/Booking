3672dbc1a2288c3cdf168328f937007ead8239b7
thanhvinh <vuthanhvinh438@gmail.com>
2024-07-19 10:46:55 +0700

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import User from './model/user'
 import connection from './config/db';
+import bodyParser from 'body-parser';
 
 import ticketRoute from './routes/ticket.route';
 
 const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 connection.connect()
 
 app.use(ticketRoute)
